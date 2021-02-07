504a8723462d3d5c9fdcb69ab1bfc643a88b3a3f
JUn <jun@jun.de>
2021-02-07 11:45:15 +0100

    fix merge problems

--- kondolenz/src/store/index.js
+++ kondolenz/src/store/index.js
@@ -6,14 +6,10 @@ const axios = require('axios')
 
 Vue.use(Vuex)
 
-<<<<<<< HEAD
 var urlQuery = '/query.php'
 var urlSave = '/save.php'
 
 var _storeMock = new Vuex.Store({
-=======
-var storeMock = new Vuex.Store({
->>>>>>> 0d708dd9c0a88afc9f76b23af82fe6426cbbf30a
   getters: {
     all_displayed_entries () {
       let pic01 = {
@@ -33,10 +29,10 @@ var storeMock = new Vuex.Store({
         caption: '04'
       }
       return [
-        {id: '0', title: 'Entry 0', author: 'a chicken', created_at: '2021-02-07 08:13:47', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', imgs: [pic01]},
-        {id: '1', title: 'Entry 1', author: 'mister bean', created_at: '2021-02-07 08:13:47', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', imgs: [pic01, pic02]},
-        {id: '2', title: 'Entry 2', author: 'i\'m elfo', created_at: '2021-02-07 08:13:47', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'},
-        {id: '3', title: 'Entry 3', author: 'sauro - the father of darkness', created_at: '2021-02-07 08:13:47', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\nStet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\najklsdjflkajfklajl;k', imgs: [pic01, pic02, pic03, pic04, pic01, pic02, pic03, pic04]}
+        {id: '0', title: 'Entry 0', author: 'a chicken', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', imgs: [pic01]},
+        {id: '1', title: 'Entry 1', author: 'mister bean', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', imgs: [pic01, pic02]},
+        {id: '2', title: 'Entry 2', author: 'i\'m elfo', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'},
+        {id: '3', title: 'Entry 3', author: 'sauro - the father of darkness', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\nStet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\najklsdjflkajfklajl;k', imgs: [pic01, pic02, pic03, pic04, pic01, pic02, pic03, pic04]}
       ]
     }
   },
@@ -97,10 +93,6 @@ var storeBackEnd = new Vuex.Store({
   }
 })
 
-<<<<<<< HEAD
 var store = storeBackEnd
-=======
-var store = storeMock
->>>>>>> 0d708dd9c0a88afc9f76b23af82fe6426cbbf30a
 
 export default store
