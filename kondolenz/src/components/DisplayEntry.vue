08a3a263fc672cab42ec2936cf87e32e5a5bbb22
merethaun <meret.unbehaun@web.de>
2021-02-07 12:05:26 +0100

    add maximise images

--- kondolenz/src/components/DisplayEntry.vue
+++ kondolenz/src/components/DisplayEntry.vue
@@ -4,21 +4,34 @@
       <h1 class="entry font title">{{ title }}</h1>
       <h1 class="entry font author">{{ author }}</h1>
     </div>
-    <div class="entry imagewrapper" v-if="imgs">
+    <div class="entry imagewrapper" v-if="(imgs)">
       <div>
         <img
           class="entry image"
-          v-for="img in imgs"
-          :key="id + '__' + img.caption"
+          v-for="(img, index) in imgs"
+          :key="id + '__' + index + '__' + img.caption"
           :src="img.path"
+          @click="openGallery(index)"
         >
       </div>
     </div>
     <p class="entry font text">{{ text }}</p>
+
+    <vue-image-lightbox-carousel
+      ref="lightbox"
+      :show="showLightbox"
+      @close="showLightbox = false"
+      :images="imgs"
+      @change="changeHighlight"
+      :showCaption="false"
+      >
+    </vue-image-lightbox-carousel>
   </div>
 </template>
 
 <script>
+import VueImageLightboxCarousel from 'vue-image-lightbox-carousel'
+
 export default {
   name: 'DisplayEntry',
   props: {
@@ -27,6 +40,41 @@ export default {
     author: String,
     id: String,
     imgs: Array
+  },
+  data () {
+    return {
+      currentIndexImage: 0,
+      showLightbox: false,
+      arrLightBox: []
+    }
+  },
+  components: {
+    VueImageLightboxCarousel
+  },
+  methods: {
+    changeHighlight (currentIndex) {
+      this.currentIndexImage = currentIndex
+      let arr = this.imgs
+      arr.map((item, index) => {
+        if (currentIndex === index) {
+          item.highlight = 1
+        } else {
+          item.highlight = 0
+        }
+        return item
+      })
+    },
+    openGallery (index) {
+      this.showLightbox = true
+      this.$refs.lightbox.showImage(index)
+    },
+    onOpenedLightBox (value) {
+      if (value) {
+        this.showLightbox = true
+      } else {
+        this.showLightbox = false
+      }
+    }
   }
 }
 </script>
